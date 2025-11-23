29bf32669356d963a0370a82c89d42cf0dfd164d
Daksh Shahani <dakshitshahani@gmail.com>
2025-11-23 01:27:27 -0500

    deleted backend folder from frontend

--- unified-search/backend/src/modules/notion/routes/search.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send("OK from Notion Auth!");
-});
-
-export default router;
--- unified-search/backend/src/routes/search.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const router = Router();
-
-router.get("/", async (req, res) => {
-  res.json({ message: "Search endpoint working!" });
-});
-
-export default router;
